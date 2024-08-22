828ea77fc4a38378e63fece478625c059d945cfb
Rubayed Ahmed <rubayed.ahmed02@gmail.com>
2024-08-22 16:47:54 +0600

    latest post data calling done

--- scripts/index.js
+++ scripts/index.js
@@ -5,13 +5,20 @@ const loadBlogPosts = async() => {
     showBlogDetails(posts)    
 }
 
+// Latest post data
+const loadLatestPosts = async() => {
+    const res = await fetch(' https://openapi.programming-hero.com/api/retro-forum/latest-posts')
+    const data = await res.json()
+    showLatestPosts(data)
+}
+
 loadBlogPosts()
+loadLatestPosts()
 
 const showBlogDetails = (blog) => {
     const cardContainer = document.getElementById('card-container');
 
     blog.forEach( data => {
-        console.log(data)
         const div = document.createElement('div')
         div.classList = 'card'
         div.innerHTML = `
@@ -49,5 +56,42 @@ const showBlogDetails = (blog) => {
                       </div>`
 
                       cardContainer.appendChild(div)
+    })
+}
+
+const showLatestPosts = (posts) => {
+    const cardContainer = document.getElementById('latest-card-container');
+
+    posts.forEach(post => {
+        console.log(post)
+
+        const div = document.createElement('div');
+        div.classList = 'card bg-base-100 w-90 shadow-xl';
+        div.innerHTML = `
+             <figure>
+                      <img
+                        src="${post?.cover_image}"
+                        alt="" />
+                    </figure>
+                    <div class="card-body">
+                        <h4 class="flex items-center gap-3 text-[#12132D99]"><i class="fa-regular fa-calendar"></i> ${post?.author?.posted_date}</h4>
+                      <h2 class="card-title text-lg font-bold">${post?.title}</h2>
+                      <p class="text-[#12132D99] text-sm">${post?.description}</p>
+
+                      <div class="card-actions flex items-center mt-3">
+                        <div class="avatar mr-4">
+                            <div class="ring-primary ring-offset-base-100 w-8 rounded-full ring ring-offset-2">
+                              <img src="${post?.profile_image}" />
+                            </div>
+                          </div>
+                        <div>
+                            <h3 class="text-lg font-bold">${post?.author?.name}</h3>
+                            <span>${post?.author?.designation}</span>
+                        </div>
+                      </div>
+                    </div>
+        `;
+        cardContainer.appendChild(div);
+
     })
 }
