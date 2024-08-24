10af5a82b5a568dd5ff3fb53fa1b5bac786c791b
Rubayed Ahmed <rubayed.ahmed02@gmail.com>
2024-08-24 20:50:53 +0600

    complete

--- scripts/index.js
+++ scripts/index.js
@@ -2,63 +2,70 @@ const loadBlogPosts = async() => {
     const res = await fetch('https://openapi.programming-hero.com/api/retro-forum/posts');
     const data = await res.json()
     const posts = data.posts;
-    showBlogDetails(posts)    
+    showBlogDetails(posts);   
 }
 
 // Latest post data
 const loadLatestPosts = async() => {
     const res = await fetch(' https://openapi.programming-hero.com/api/retro-forum/latest-posts')
     const data = await res.json()
-    showLatestPosts(data)
+    showLatestPosts(data);
 }
 
-loadBlogPosts()
+// loadBlogPosts()
 loadLatestPosts()
 
 const showBlogDetails = (blog) => {
+    console.log(blog.posts)
     const cardContainer = document.getElementById('card-container');
 
-    blog.forEach( data => {
+    blog.posts.forEach( data => {
         const div = document.createElement('div')
-        div.classList = 'card'
+        div.classList = 'card';
+        
         div.innerHTML = `
         <div class="card-body bg-[#F3F3F5] my-4 rounded-lg">
-                        <div class="flex gap-2">
-                        <div class="avatar online mr-4">
-                        <div class="w-16 rounded">
-                            <img
-                            src="${data?.image}"
-                            alt="Tailwind-CSS-Avatar-component" />
-                        </div>
-                        </div>
-                          <div>
-                            <h2 class="card-title">${data?.title}</h2>
-                            <p class="text-[#12132D99] mt-3">${data?.description}</p>
-                          </div>
-                        </div>
-                        <div class="border my-4 border-dotted"></div>
-                        <div class="card-actions justify-end">
-                          <div class="stats gap-4 bg-[#F3F3F5]">
-                            <div class="stat">
-                              <div class="stat-figure text-primary">${data?.comment_count}</div>
-                              <div class="stat-title">Comments</div>
-                            </div>
-                            <div class="stat">
-                              <div class="stat-figure text-secondary">${data?.view_count}</div>
-                              <div class="stat-title">Views</div>
-                            </div>
-                            <div class="stat">
-                              <div class="stat-figure text-accent">${data?.posted_time} min</div>
-                              <div class="stat-title">Read</div>
-                            </div>
-                            <div class="self-center">
-                                <button id="show-title-btn" class="bg-[#10B981] rounded-full py-1 px-2 text-white"><i class="fa-regular fa-envelope""></i></button>
-                            </div>
-                          </div>
-                        </div>
-                      </div>`
-
-                      cardContainer.appendChild(div);
+        <div class="flex gap-2">
+        <div class="avatar online mr-4">
+        <div class="w-16 rounded">
+        <img
+        src="${data?.image}"
+        alt="Tailwind-CSS-Avatar-component" />
+        </div>
+        </div>
+        <div>
+        <span># ${data?.category}</span>
+        <h2 class="card-title">${data?.title}</h2>
+        <p class="text-[#12132D99] mt-3">${data?.description}</p>
+        </div>
+        </div>
+        <div class="border my-4 border-dotted"></div>
+        <div class="card-actions justify-end">
+        <div class="stats gap-4 bg-[#F3F3F5]">
+        <div class="stat">
+        <div class="stat-figure text-primary">${data?.comment_count}</div>
+        <div class="stat-title">Comments</div>
+        </div>
+        <div class="stat">
+        <div class="stat-figure text-secondary">${data?.view_count}</div>
+        <div class="stat-title">Views</div>
+        </div>
+        <div class="stat">
+        <div class="stat-figure text-accent">${data?.posted_time} min</div>
+        <div class="stat-title">Read</div>
+        </div>
+        <div class="self-center">
+        <button onclick="showPostTitle()" class="bg-[#10B981] rounded-full py-1 px-2 text-white"><i class="fa-regular fa-envelope""></i></button>
+        </div>
+        </div>
+        </div>
+        </div>`
+        
+        cardContainer.appendChild(div);
+        // if(data.category === "Comedy") {
+        // }
+        
+        
     })
 }
 
@@ -96,4 +103,22 @@ const showLatestPosts = (posts) => {
         cardContainer.appendChild(div);
 
     })
+}
+
+
+const searchBtn = () => {
+    const searchInputValue = document.getElementById('search-input');
+    const searchText = searchInputValue.value;
+    loadDataByCategory(searchText)
+    
+}
+
+const loadDataByCategory = async(searchText) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts?category=${searchText}`);
+    const data = await res.json();
+    showBlogDetails(data)
+}
+
+const showPostTitle = () => {
+    console.log("Clicked")
 }
